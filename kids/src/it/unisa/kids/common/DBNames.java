178081c3665163e56a48a897c1b1914cf821d3a1
navipar@gmail.com <navipar@gmail.com@a86227fa-c5a3-d7f0-fd0c-0a7fbeb29eb0>
2012-11-10 16:41:46 +0000

    Aggiunto package common e la classe DBNames che raccoglie i nomi del db in comune ai tre sottosistemi. Mancano gli attributi. Chiedo ai manager cosa ne pensano, credo sia molto utile, invito a tutti di usare tali riferimenti per il momento, se ci sono errori o nomi non corretti provvedete a modificare :)

---
+++ kids/src/it/unisa/kids/common/DBNames.java
@@ -0,0 +1,35 @@
+package it.unisa.kids.common;
+
+public final class DBNames {
+	
+	private DBNames() {
+		
+	}
+
+	public static final String TABLE_ACCOUNT = "account";
+	public static final String TABLE_RECOURSE = "recourse";
+	public static final String TABLE_RINUNCIA = "";		// da completare
+	public static final String TABLE_CLASSIFICATION = "classification";
+	public static final String TABLE_CLASS = "class";
+	public static final String TABLE_CHILD = "child_inscription";
+	public static final String TABLE_ESITO = "";		// da completare
+	public static final String TABLE_ASSIGNATION = "assignation";
+	
+	public static final String TABLE_REQUEST = "request";
+	public static final String TABLE_DIFF_MENU = "diff_menu";
+	public static final String TABLE_REFUND = "refund";
+	public static final String TABLE_PAYMENT = "payment";
+	public static final String TABLE_TRAINEE = "trainee";
+	public static final String TABLE_TRAINEE_ACT = "trainee_activity";
+	
+	public static final String TABLE_PARTICIPATION = "participation";
+	public static final String TABLE_COMUNICATION = "comunication";
+	public static final String TABLE_REUNION = "reunion";
+	public static final String TABLE_NEWS = "news";
+	public static final String TABLE_ANNUAL_PROJ = "annual_proj";
+	public static final String TABLE_SECTION_PROJ = "section_proj";
+	public static final String TABLE_COMMENT = "comment";
+	public static final String TABLE_ACT = "activity";
+	public static final String TABLE_DAILY_SECTION_ACT = "daily_section_act";
+	public static final String TABLE_CHILD_PARTICIPATION = "child_participation";
+}
