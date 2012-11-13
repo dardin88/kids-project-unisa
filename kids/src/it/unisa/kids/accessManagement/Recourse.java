7317d8ab5493dc7128a626ccf739a91f58f5c8d1
gianmarco.delpozzo2@gmail.com <gianmarco.delpozzo2@gmail.com@a86227fa-c5a3-d7f0-fd0c-0a7fbeb29eb0>
2012-11-13 11:09:26 +0000

    aggiunta documentazione JavaDoc.

--- kids/src/it/unisa/kids/accessManagement/Recourse.java
+++ kids/src/it/unisa/kids/accessManagement/Recourse.java
@@ -1,53 +1,105 @@
 package it.unisa.kids.accessManagement;
 
+/**
+ * This class models the Account entity
+ * @author Gianmarco Del Pozzo
+ *
+ */
+
+
 public class Recourse {
 	private int id;
-	private String data;
+	private String date;
 	private String reason;
 	private String valutation;
 	private int id_registration;
+	
 
-	public void setId_registration(int id_registration) {
-		this.id_registration = id_registration;
+	/**
+	 * Empty constructor 
+	 */
+	
+	public Recourse() {
 	}
 
+
+	/**this method sets the id registration linked with a recourse
+	 * @param String pId_registration
+	 */
+	public void setId_registration(int pId_registration) {
+		this.id_registration = pId_registration;
+	}
+	
+	/**this method returns the id registration linked with a recourse
+	 * @return String id_registrazion
+	 */
+	
 	public int getId_registration() {
 		return id_registration;
 	}
-
-	public Recourse() {
-	}
+	
+	/**this method returns the id of a recourse
+	 * @return String id
+	 */
 
 	public int getId() {
 		return id;
 	}
+	
+	/**this method returns the date when the recourse was made
+	 * @return String data
+	 */
 
 	public String getData() {
-		return data;
+		return date;
 	}
+	
+	/**this method returns the reason of a recourse
+	 * @return String reason
+	 */
 
 	public String getReason() {
 		return reason;
 	}
+	
+	/**this method returns the valutation of a recourse
+	 * @return String valutation
+	 */
 
 	public String getValutation() {
 		return valutation;
 	}
-
-	public void setId(int id) {
-		this.id = id;
+	
+	/**this method set the id of a recourse
+	 * @param String pId
+	 */
+	
+	public void setId(int pId) {
+		this.id = pId;
 	}
-
-	public void setData(String data) {
-		this.data = data;
+	
+	/**this method set the date when the recourse was made
+	 * @param String pDate
+	 */
+	
+	public void setData(String pDate) {
+		this.date = pDate;
 	}
-
-	public void setReason(String motivo) {
-		this.reason = motivo;
+	
+	/**this method set the reason of recourse
+	 * @param String pReason
+	 */
+	
+	public void setReason(String pReason) {
+		this.reason = pReason;
 	}
+	
+	/**this method set the valutation of a recourse
+	 * @param String pValutation
+	 * 	 */
 
-	public void setValutation(String valutazione) {
-		this.valutation = valutazione;
+	public void setValutation(String pValutation) {
+		this.valutation = pValutation;
 	}
 
 }
