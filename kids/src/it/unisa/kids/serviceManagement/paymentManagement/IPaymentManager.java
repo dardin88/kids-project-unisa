af1921b7b6631450769d836880bec62ca3bae59b
navipar@gmail.com <navipar@gmail.com@a86227fa-c5a3-d7f0-fd0c-0a7fbeb29eb0>
2012-11-13 19:49:13 +0000

    Rimossi alcuni riferimenti a MandatoryFieldException Implementato il metodo JDBCPaymentManager.update

--- kids/src/it/unisa/kids/serviceManagement/paymentManagement/IPaymentManager.java
+++ kids/src/it/unisa/kids/serviceManagement/paymentManagement/IPaymentManager.java
@@ -1,16 +1,14 @@
 package it.unisa.kids.serviceManagement.paymentManagement;
 
-import it.unisa.kids.common.exception.MandatoryFieldException;
-
 import java.sql.SQLException;
 import java.util.List;
 
 public interface IPaymentManager {
 
-	public void insert(Payment pPayment) throws SQLException, MandatoryFieldException;
-	public void update(Payment pPayment) throws SQLException, MandatoryFieldException;
-	public void delete(Payment pPayment) throws SQLException, MandatoryFieldException;
+	public void insert(Payment pPayment) throws SQLException;
+	public void update(Payment pPayment) throws SQLException;
+	public void delete(Payment pPayment) throws SQLException;
 	
-	public List<Payment> getPaymentsByObject(Payment pObjectPayment) throws SQLException, MandatoryFieldException;
-	public List<Payment> getPaymentList() throws SQLException, MandatoryFieldException;
+	public List<Payment> getPaymentsByObject(Payment pObjectPayment) throws SQLException;
+	public List<Payment> getPaymentList() throws SQLException;
 }
