78e9c3ff98d81ea056ad9f7a1c644e873e1ce43b
gianmarco.delpozzo2@gmail.com <gianmarco.delpozzo2@gmail.com@a86227fa-c5a3-d7f0-fd0c-0a7fbeb29eb0>
2012-11-15 09:48:06 +0000

    implementato il manager usando Bridge Pattern

---
+++ kids/src/it/unisa/kids/accessManagement/accountManagement/RefinedAbstractAccountManager.java
@@ -0,0 +1,14 @@
+package it.unisa.kids.accessManagement.accountManagement;
+
+import it.unisa.kids.common.AbstractManager;
+
+
+public class RefinedAbstractAccountManager extends AbstractManager<IAccountManager> {
+	public IAccountManager getManagerImplementor() {
+		this.imp = JDBCManagerAccount.getInstance();
+		return this.imp;
+	}
+
+	
+	
+}
