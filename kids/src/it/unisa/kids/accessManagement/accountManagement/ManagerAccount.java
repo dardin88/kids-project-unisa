9a57cf2748801775879d76941ff58c2b81b865a3
gianmarco.delpozzo2@gmail.com <gianmarco.delpozzo2@gmail.com@a86227fa-c5a3-d7f0-fd0c-0a7fbeb29eb0>
2012-11-15 09:07:01 +0000

    Elimanato import libreria " glassfish "  O.O

--- kids/src/it/unisa/kids/accessManagement/accountManagement/ManagerAccount.java
+++ kids/src/it/unisa/kids/accessManagement/accountManagement/ManagerAccount.java
@@ -8,10 +8,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
-import java.util.List;
 import java.util.Random;
-
-import com.sun.org.glassfish.external.arc.Taxonomy;
 
 public class ManagerAccount {
 
