0a8d50f390c80c2f5b13c60ca9d7685c33dfc43e
pasquale.caldarese@gmail.com <pasquale.caldarese@gmail.com@a86227fa-c5a3-d7f0-fd0c-0a7fbeb29eb0>
2012-11-16 17:23:41 +0000

    Cambiato i nomi alle classi

--- kids/src/it/unisa/kids/communicationManagement/programEducativeManagement/ActivitySectionDaily.java
+++ kids/src/it/unisa/kids/communicationManagement/programEducativeManagement/ActivitySectionDaily.java
@@ -8,7 +8,7 @@ import java.util.GregorianCalendar;
  * @author Francesco Di Lorenzo
  *
  */
-public class ActivitySectionDaily extends ProjectAnnualSection 
+public class ActivitySectionDaily extends AnnualProjectSection 
 {
 	
 	private int id;
