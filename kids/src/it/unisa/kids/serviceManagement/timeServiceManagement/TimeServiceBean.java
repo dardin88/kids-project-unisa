b573804146300cbee47396910c7705b1f54870a8
navipar@gmail.com <navipar@gmail.com@a86227fa-c5a3-d7f0-fd0c-0a7fbeb29eb0>
2012-11-19 15:18:24 +0000

    Aggiunti i Bean e il Manager della gestione degli orari di servizio utilizzando il Facade della gestione comunicazioni (ancora da definire).

---
+++ kids/src/it/unisa/kids/serviceManagement/timeServiceManagement/TimeServiceBean.java
@@ -0,0 +1,9 @@
+package it.unisa.kids.serviceManagement.timeServiceManagement;
+
+import it.unisa.kids.communicationManagement.newsManagement.News;
+
+public class TimeServiceBean extends News {
+	
+	public static final String OPENING_TIMESERV = "opening_time";
+	public static final String CLOSING_TIMESERV = "closing_time";
+}
