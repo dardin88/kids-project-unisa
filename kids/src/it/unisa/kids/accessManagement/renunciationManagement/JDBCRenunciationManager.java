8bdd493facfb0158a8d8c7bae92a83d6ab76e715
a.porcelli90@gmail.com <a.porcelli90@gmail.com@a86227fa-c5a3-d7f0-fd0c-0a7fbeb29eb0>
2012-11-21 09:23:27 +0000

    modifiche alle query cambio del nome in jdbcrenunciationmanager implementata l'interfaccia irenunciationmanager

--- kids/src/it/unisa/kids/accessManagement/renunciationManagement/JDBCRenunciationManager.java
+++ kids/src/it/unisa/kids/accessManagement/renunciationManagement/JDBCRenunciationManager.java
@@ -10,51 +10,60 @@ import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
-public class JDBCRenunciationManager {
+public class JDBCRenunciationManager implements IRenunciationManager{
 	
 	private static JDBCRenunciationManager manager;
 
-	private JDBCRenunciationManager() {
-	}
+	  private JDBCRenunciationManager()  {  }
 
-	public static JDBCRenunciationManager getInstance() {
-		if (manager == null) {
+	  public static JDBCRenunciationManager getInstance()
+	  {
+	    if (manager == null)
+	    {
 	      manager = new JDBCRenunciationManager();
 	    }
-		return manager;
-	}
+
+	    return manager;
+	  }
 	  
-	public Renunciation create(Renunciation pWaiver) throws SQLException {
-		Connection con = null;
-		Statement stmt=null;
-		
-		try {
-			con=DBConnectionPool.getConnection();
-			String query="INSERT INTO '"+DBNames.TABLE_RINUNCIA+"' ("+DBNames.ATT_RINUNCIA_DATE+" , "+DBNames.ATT_RINUNCIA_CONFIRM+") " +
-					"VALUES"+		
-	  				"('"+pWaiver.getDate()+
-	  				"', '"+pWaiver.isConfirmation()+"' );"; 			
-			stmt = con.createStatement();
-			stmt.executeUpdate(query);
-		} finally {
+	  public Renunciation create(Renunciation pWaiver) throws SQLException
+	  {
+		  Connection con = null;
+		  Statement stmt=null;
+			
+		  try
+		  {
+			  con=DBConnectionPool.getConnection();
+			  String query="INSERT INTO '"+DBNames.TABLE_RINUNCIA+"' ("+DBNames.ATT_RINUNCIA_DATE+" , "+DBNames.ATT_RINUNCIA_CONFIRM+") " +
+			  		"VALUES"+		
+	  					"('"+pWaiver.getDate()+"', '"+
+	  						 pWaiver.isConfirmation()+"' );";
+			  
+			  stmt = con.createStatement();
+			  stmt.executeUpdate(query);
+		  }
+		  finally{
 			stmt.close();
 			DBConnectionPool.releaseConnection(con);
-		}
+		  }
+		  
+		  return pWaiver;
+	  }
 	  
-		return pWaiver;
-	}
-	  
-	public Renunciation delete(Renunciation pWaiver) throws SQLException {
+	  public Renunciation delete(Renunciation pWaiver) throws SQLException
+	  {
 		  Connection con = null;
 		  Statement stmt=null;
 		  
-		  try {
-			  con = DBConnectionPool.getConnection();
-			  String query = "DELETE FROM '"+DBNames.TABLE_RINUNCIA+"' WHERE '"+DBNames.ATT_RINUNCIA_ID+"'='"+pWaiver.getId()+"';";
+		  try
+		  {
+			  con=DBConnectionPool.getConnection();
+			  String query="DELETE FROM '"+DBNames.TABLE_RINUNCIA+"' WHERE '"+DBNames.ATT_RINUNCIA_ID+"'='"+pWaiver.getId()+"';";
 			  stmt = con.createStatement();
 			  stmt.executeUpdate(query);
-		  } finally {
-			  stmt.close(); // controllare prima se la connessione è avvenuta con successo e solo in tal caso chiuderla
+		  }
+		  finally{
+			  stmt.close();
 			  DBConnectionPool.releaseConnection(con);
 		  }
 		  
@@ -62,61 +71,82 @@ public class JDBCRenunciationManager {
 	  }
 	  
 	  
-	public List<Renunciation> search(Renunciation pWaiver) throws SQLException {
-		Connection con = null;
-		Statement stmt=null;
-		ResultSet result=null;
-		Renunciation tmpWaiver=new Renunciation();
-		List<Renunciation> listOfWaiver=new ArrayList<Renunciation>();		
-		String query="SELECT * FROM '"+DBNames.TABLE_RINUNCIA+"' WHERE ";				
+	  public List<Renunciation> search(Renunciation pWaiver) throws SQLException
+	  {
+		  Connection con = null;
+		  Statement stmt=null;
+		  ResultSet result=null;
+		  Renunciation tmpWaiver=new Renunciation();
+		  boolean andState = false;
+		  List<Renunciation> listOfWaiver=new ArrayList<Renunciation>();		
+		  String query="SELECT * FROM '"+DBNames.TABLE_RINUNCIA+"' WHERE ";				
 		  
-		if(pWaiver.getId()!=null)
-			query=query+"'"+DBNames.ATT_RINUNCIA_ID+"'='"+pWaiver.getId()+"'";
-		if(pWaiver.getDate()!=null)
-			query=query+"'"+DBNames.ATT_RINUNCIA_DATE+"'='"+pWaiver.getDate()+"'"; 
-		if(pWaiver.isConfirmation()==true)									//Chiedo conferma :D
-			query=query+"'"+DBNames.ATT_RINUNCIA_CONFIRM+"'='"+pWaiver.isConfirmation()+"'";
-		  
-		query=query+";";
-		  
-		  //ci vogliono gli and
+		  if (pWaiver.getId()!=null)
+		  {
+			  query+="'"+DBNames.ATT_RINUNCIA_ID+"'='"+pWaiver.getId()+"'";
+		  	  andState=true;
+		  }
+		  if (pWaiver.getDate()!=null)
+		  {
+			  query+=useAnd(andState)+"'"+DBNames.ATT_RINUNCIA_DATE+"'='"+pWaiver.getDate()+"'";
+		  	  andState=true;
+		  }
+		  if (pWaiver.isConfirmation()==true)									//Chiedo conferma :D
+		  {
+			  query+=useAnd(andState)+"'"+DBNames.ATT_RINUNCIA_CONFIRM+"'='"+pWaiver.isConfirmation()+"'";
+		  	  andState=true;
+		  }
+		  query+=";";
 		  		
-		try {
-			con=DBConnectionPool.getConnection();
-			stmt = con.createStatement();
-			result=stmt.executeQuery(query);
+		  try
+		  {
+			  con=DBConnectionPool.getConnection();
+			  stmt = con.createStatement();
+			  result=stmt.executeQuery(query);
 			  
-			while(result.next()) {
-				tmpWaiver.setConfirmation(result.getBoolean(DBNames.ATT_RINUNCIA_CONFIRM));
-				tmpWaiver.setDate(result.getString(DBNames.ATT_RINUNCIA_DATE));
-				tmpWaiver.setId(result.getString(DBNames.ATT_RINUNCIA_ID));
-			
-				listOfWaiver.add(tmpWaiver);
-			}
-		} finally {
-			stmt.close();
-			DBConnectionPool.releaseConnection(con);
+			  while(result.next())
+			  {
+				  tmpWaiver.setConfirmation(result.getBoolean(DBNames.ATT_RINUNCIA_CONFIRM));
+				  tmpWaiver.setDate(result.getString(DBNames.ATT_RINUNCIA_DATE));
+				  tmpWaiver.setId(result.getString(DBNames.ATT_RINUNCIA_ID));
+				  
+				  listOfWaiver.add(tmpWaiver);
+			  }
+		  }
+		  finally{
+			  stmt.close();
+			  DBConnectionPool.releaseConnection(con);
+		  }
+		  
+		  return listOfWaiver;
+	  }
+	  
+		private String useAnd(boolean pEnableAnd) {
+			return pEnableAnd ? " AND " : " ";
 		}
-		return listOfWaiver;
-	}
-	
-	public Renunciation modify(Renunciation pWaiver) throws SQLException {
-		Connection con = null;
-		Statement stmt=null;
-		String query="UPDATE '"+DBNames.TABLE_RINUNCIA+"' " + 	
-					"SET ("+DBNames.ATT_RINUNCIA_DATE+" , "+DBNames.ATT_RINUNCIA_CONFIRM+") VALUES"+ 
-					"('"+pWaiver.getDate()+
-  					"', '"+pWaiver.isConfirmation()+"' )"+ 
-					"WHERE '"+DBNames.ATT_RINUNCIA_ID+"'="+pWaiver.getId()+";"; 
+	  
+	  public Renunciation update(Renunciation pWaiver)throws SQLException
+	  {
+		  	Connection con = null;
+		  	Statement stmt=null;
+			String query="UPDATE '"+DBNames.TABLE_RINUNCIA+"' " + 	
+							"SET ("+DBNames.ATT_RINUNCIA_DATE+" , "+DBNames.ATT_RINUNCIA_CONFIRM+") VALUES"+ 
+									"('"+pWaiver.getDate()+"', '"+
+										 pWaiver.isConfirmation()+"' )"+ 
+							"WHERE '"+DBNames.ATT_RINUNCIA_ID+"'="+pWaiver.getId()+";"; 
 			
-		try {
-			con=DBConnectionPool.getConnection();
-			stmt = con.createStatement();
-			stmt.executeUpdate(query);
-		} finally {
-			stmt.close();
-			DBConnectionPool.releaseConnection(con);
-		}	
-		return pWaiver;
-	}
+			try
+			  {
+				  con=DBConnectionPool.getConnection();
+				  stmt = con.createStatement();
+				  stmt.executeUpdate(query);
+			  }
+			  finally{
+				  stmt.close();
+				  DBConnectionPool.releaseConnection(con);
+			  }	
+			
+			return pWaiver;
+		}
 }
+
