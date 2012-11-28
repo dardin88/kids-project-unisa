6f3cfca6058302dceed435dc08720c31a8d7954f
gianmarco.delpozzo2@gmail.com <gianmarco.delpozzo2@gmail.com@a86227fa-c5a3-d7f0-fd0c-0a7fbeb29eb0>
2012-11-28 13:17:57 +0000

    Aggiunte Metodi Façade

--- kids/src/it/unisa/kids/common/IAccessFacade.java
+++ kids/src/it/unisa/kids/common/IAccessFacade.java
@@ -1,13 +1,19 @@
 package it.unisa.kids.common;
 
+import it.unisa.kids.accessManagement.accountManagement.Account;
 import java.sql.SQLException;
 
 import it.unisa.kids.accessManagement.registrationChildManagement.RegistrationChild;
+import java.util.List;
 
 public interface IAccessFacade 
 {
 	public int getParentId(int childId);
 	public int getNumberOfChildren(int parentId) throws SQLException;
-	public boolean isSick(RegistrationChild c);	
+	public boolean isSick(RegistrationChild c);
+        public Account insert(Account pAccount);
+        public Account update(Account pAccount);
+        public List<Account> search(Account pAccount);
+        public Account delete(Account pAccount);
 
 }
