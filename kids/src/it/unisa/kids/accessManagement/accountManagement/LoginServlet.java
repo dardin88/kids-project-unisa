2e8d9cef1d0cd670ebc663292d3f8d5c63f60309
dilorfrancesco@gmail.com <dilorfrancesco@gmail.com@a86227fa-c5a3-d7f0-fd0c-0a7fbeb29eb0>
2012-11-28 14:44:28 +0000

    inserita loginservlet e aggiunto metodo autenticate nel jdbcaccountmanager

---
+++ kids/src/it/unisa/kids/accessManagement/accountManagement/LoginServlet.java
@@ -0,0 +1,93 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package it.unisa.kids.accessManagement.accountManagement;
+
+import it.unisa.kids.accessManagement.accountManagement.*;
+import it.unisa.kids.common.DBNames;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author francesco
+ */
+public class LoginServlet extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP
+     * <code>GET</code> and
+     * <code>POST</code> methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        try {
+            String username = request.getParameter(DBNames.ATT_ACCOUNT_NICKNAME);
+            String password = request.getParameter(DBNames.ATT_ACCOUNT_PASSWORD);
+            JDBCAccountManager mn = JDBCAccountManager.getInstance();
+            Account user = mn.authenticate(username, password);
+            if (user != null) {
+                request.getSession().setAttribute("user", user);
+            } else {
+                request.setAttribute("error", "Verifica nome utente e password.");
+            }
+            request.getServletContext().getRequestDispatcher("/index.jsp").forward(request, response);
+        } catch (Exception ex) {
+            Logger.getLogger(LoginServlet.class.getName()).log(Level.SEVERE,null, ex);
+        } 
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP
+     * <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+       processRequest(request, response);
+       
+    }
+
+    /**
+     * Handles the HTTP
+     * <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+}
