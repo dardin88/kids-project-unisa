f0df23bddc24d278decda55358f0ea9143291ea8
navipar@gmail.com <navipar@gmail.com@a86227fa-c5a3-d7f0-fd0c-0a7fbeb29eb0>
2012-11-28 16:02:12 +0000

    Aggiunto metodo nel CommunicationFacade e sistemate alcuini metodi del timeservice management

--- kids/src/it/unisa/kids/common/bean/TimeServiceBean.java
+++
@@ -1,9 +0,0 @@
-package it.unisa.kids.common.bean;
-
-import it.unisa.kids.communicationManagement.newsManagement.News;
-
-public class TimeServiceBean extends News {
-	
-	public static final String OPENING_TIMESERV = "opening_time";
-	public static final String CLOSING_TIMESERV = "closing_time";
-}
