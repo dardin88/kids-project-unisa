df9ea19a07850ec1355c2ee26edc352df96299eb
g.alfierimail <g.alfierimail@a86227fa-c5a3-d7f0-fd0c-0a7fbeb29eb0>
2012-11-28 14:30:47 +0000

    implementati i metodi per supportare i commenti

--- kids/src/it/unisa/kids/communicationManagement/programEducationalManagement/AnnualProject.java
+++ kids/src/it/unisa/kids/communicationManagement/programEducationalManagement/AnnualProject.java
@@ -1,5 +1,8 @@
 package it.unisa.kids.communicationManagement.programEducationalManagement;
 
+import java.util.ArrayList;
+import java.util.Map.Entry;
+
 /**
  * The class model the new entity ProjectAnnual
  * @author Francesco Di Lorenzo
@@ -11,13 +14,57 @@ public class AnnualProject
 	private String topic;
 	private String description;
 	private String applicationYear;
+        private ArrayList<Entry> commenti;
 	private int id;
+        
+        
+        public class Comment<K,V> implements Entry<String,String>{
+            private String author;
+            private String comment;
+            
+            public Comment(String author, String comment){
+                this.author=author;
+                this.comment=comment;
+                
+            }
+        /**
+	 * this method return the Author of the comment about the project annual
+	 * @return String Author
+	 */
+        public String getKey() {
+            return this.author;
+        }
+        /**
+	 * this method return the text of the Comment 
+	 * @return String Comment
+	 */
+        public String getValue() {
+            return this.comment;
+        }
+
+	/**
+	 * this method set the text of the comment
+	 * @param comment
+         * @return String author
+	 */
+
+        public String setValue(String comment) {
+            this.comment=comment;
+            return this.author;
+        }
+        
+        public String toString(){
+            return this.author +" "+ this.comment;
+        }
+            
+        }
 	
 	/**
-	 * the empty costructor
+	 * the constructor inizializes the container of the comments
 	 */
 	public AnnualProject()
 	{	
+            commenti=new ArrayList();
 	}
 
 	/**
@@ -89,6 +136,37 @@ public class AnnualProject
 	public int getId(){
 		return id;
 	}
-	
+        /**
+	 * this method insert the text of the comment in the ArrayList
+	 * @param comment
+	 */
+        public synchronized void addComment(String author, String comment){
+            this.commenti.add(new Comment(author, comment));
+            }
+	/**
+	 * this method return the text of the comment in the ArrayList at index index
+         * @param index
+	 * @return String text of comment
+	 */
+        public synchronized String getComment(int index){
+            return commenti.get(index).getValue().toString();
+            
+        }
+	/**
+	 * this method return the author of the comment in the ArrayList at index index
+         * @param index
+	 * @return String author
+	 */
+        public synchronized String getAuthor(int index){
+            return commenti.get(index).getKey().toString();
+        }
+        /**
+	 * this method return the number of the comments in the ArrayList
+         *
+	 * @return int size
+	 */
+        public synchronized int getCommentNum(){
+            return commenti.size();
+        }
 
 }
