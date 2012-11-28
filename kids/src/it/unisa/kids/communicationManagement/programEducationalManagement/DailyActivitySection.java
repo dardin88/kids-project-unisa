502d56d3b0a68318b6ff62d57f9b903ef602578c
g.alfierimail <g.alfierimail@a86227fa-c5a3-d7f0-fd0c-0a7fbeb29eb0>
2012-11-28 15:51:04 +0000

    prima bozza per consentire l'introduzione e la modifica delle presenze dei bambini ad ogni attività giornaliera

--- kids/src/it/unisa/kids/communicationManagement/programEducationalManagement/DailyActivitySection.java
+++ kids/src/it/unisa/kids/communicationManagement/programEducationalManagement/DailyActivitySection.java
@@ -1,11 +1,16 @@
 package it.unisa.kids.communicationManagement.programEducationalManagement;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.GregorianCalendar;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
 
 /**
  * this class extends ProjectAnnualSection
  * model an activity daily for section
- * @author Francesco Di Lorenzo
+ * @author Francesco Di Lorenzo, Giuseppe Alfieri
  *
  */
 public class DailyActivitySection extends AnnualProjectSection 
@@ -16,6 +21,66 @@ public class DailyActivitySection extends AnnualProjectSection
 	private int idEducator;
 	private GregorianCalendar data;
 	private String notes;
+        private ChildrenList lista;
+        
+        public class ChildrenList{
+            public class ChildrenField<String, Boolean> implements Entry<String,Boolean>{
+            String name;
+            Boolean presence;
+            
+            public ChildrenField(){
+                
+            }
+                
+         /**
+	 * this method return the Name of the baby
+         * @return String Author
+	 */
+            public String getKey() {
+                return this.name;
+            }
+         /**
+	 * this method return true if the child was present false otherwise. 
+         * @return Boolean present?
+	 */
+            public Boolean getValue() {
+                return this.presence;
+            }
+
+	 /**
+	 * this method set the value of presence
+	 * @param present?
+         * @return boolean new value
+	 */
+            public Boolean setValue(Boolean value) {
+                this.presence=value;
+                return this.presence;
+            }
+            
+            }
+            
+            private ArrayList<ChildrenField> container;
+            
+            public ChildrenList(){
+                container=new ArrayList<ChildrenField>();
+                
+            }
+            
+            public int size(){
+                return container.size();
+            }
+            
+            public boolean insertList(int sectionId){
+                
+                /* metodo che prende in input l'Id della sezione e inserisce tutti i bambini della sezione nel registro
+                 * 
+                 */
+            }
+            public void setPresence(String name){
+                
+            }
+            
+        }
 	
 	/**
 	 * this method return the idActivity of the activity section daily
