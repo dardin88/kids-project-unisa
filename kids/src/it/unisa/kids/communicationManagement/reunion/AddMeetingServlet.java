10cec0c5672f01382d92d0c5f47d0904cdc1c174
pasquale.caldarese@gmail.com <pasquale.caldarese@gmail.com@a86227fa-c5a3-d7f0-fd0c-0a7fbeb29eb0>
2012-12-01 20:18:44 +0000

    Gestione Riunioni

---
+++ kids/src/it/unisa/kids/communicationManagement/reunion/AddMeetingServlet.java
@@ -0,0 +1,94 @@
+package it.unisa.kids.communicationManagement.reunion;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Date;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.GregorianCalendar;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Pasquale
+ */
+public class AddMeetingServlet extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP
+     * <code>GET</code> and
+     * <code>POST</code> methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+        try {
+            JDBCReunionManager am = JDBCReunionManager.getInstance();
+            Reunion meeting = new Reunion();
+
+            meeting.setTitle(request.getParameter("meetingTitolo"));
+            meeting.setDescription(request.getParameter("meetingDescrizione"));
+            meeting.setDate(request.getParameter("meetingData"));
+            meeting.setFirstTime(request.getParameter("meetingOraInizio"));
+            meeting.setSecondTime(request.getParameter("meetingOraFine"));
+            meeting.setType(request.getParameter("meetingTipo"));
+            am.insert(meeting);
+            request.getServletContext().getRequestDispatcher("/meetingCalendar.jsp").forward(request, response);
+        } catch (Exception ex) {
+            Logger.getLogger(AddMeetingServlet.class.getName()).log(
+                    Level.SEVERE, null, ex);
+        }
+    } // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+
+    /**
+     * Handles the HTTP
+     * <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP
+     * <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+}
