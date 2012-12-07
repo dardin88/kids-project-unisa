4d0a7284c2e1027785840b1d9117549da98d2ecb
djst3z@gmail.com <djst3z@gmail.com@a86227fa-c5a3-d7f0-fd0c-0a7fbeb29eb0>
2012-12-07 14:09:05 +0000

    Aggiunta sezione Visualizza orario di servizio e richiesta orario di servizio in timeService.jsp per l attore Genitore

--- kids/WebContent/js/timeService.js
+++ kids/WebContent/js/timeService.js
@@ -5,3 +5,30 @@
 function initializetimeServicePage() {
     $("#timeserviceTab").tabs();
 }
+
+function buildnotifyTable(){
+    $('#notifyTable').dataTable({
+        "bProcessing": true,
+        "oLanguage": {
+            "sLengthMenu": "Mostra _MENU_ oggetti",
+            "sSearch": "Cerca:",
+            "sInfo": "Da _START_ a _END_ di _TOTAL_ oggetti",
+            "sZeroRecords": "Nessun oggetto trovato",
+            "sEmptyTable": "Nessun oggetto trovato",
+            "oPaginate": {
+                "sFirst": "Prima",
+                "sLast": "Ultima",
+                "sNext": "Successiva",
+                "sPrevious": "Precedente"
+            }
+        },
+        "bJQueryUI": true,
+        "bPaginate": true,
+        "bLengthChange": true,
+        "bFilter": true,
+        "bSort": true,
+        "bInfo": true,
+        "bAutoWidth": false,
+        "sPaginationType": "full_numbers"
+    });
+}
