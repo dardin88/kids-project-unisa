97de39ef3a7469e9f450d1c1ab9697d7b9a6fbdd
pasquale.caldarese@gmail.com <pasquale.caldarese@gmail.com@a86227fa-c5a3-d7f0-fd0c-0a7fbeb29eb0>
2012-12-10 19:56:53 +0000

    Cambiato nome pacchetto da reunion a meeting

---
+++ kids/src/it/unisa/kids/communicationManagement/meeting/ErroreNeiDati.java
@@ -0,0 +1,12 @@
+package it.unisa.kids.communicationManagement.meeting;
+
+public class ErroreNeiDati extends Exception {
+
+	public ErroreNeiDati() {
+		super();
+	}
+	
+	public ErroreNeiDati(String msg) {
+		super(msg);
+	}
+}
