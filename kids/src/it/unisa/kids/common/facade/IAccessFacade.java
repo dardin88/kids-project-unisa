fef708c665c4895e70913ba04a690e17d7fb3ef3
navipar@gmail.com <navipar@gmail.com@a86227fa-c5a3-d7f0-fd0c-0a7fbeb29eb0>
2012-12-10 03:06:44 +0000

    Aggiunti e implementati alcuni metodi nell'AccessFacade

--- kids/src/it/unisa/kids/common/facade/IAccessFacade.java
+++ kids/src/it/unisa/kids/common/facade/IAccessFacade.java
@@ -1,21 +1,31 @@
-    package it.unisa.kids.common.facade;
+package it.unisa.kids.common.facade;
 
 import it.unisa.kids.accessManagement.accountManagement.Account;
+import it.unisa.kids.accessManagement.classManagement.ClassBean;
 import java.sql.SQLException;
 
 import it.unisa.kids.accessManagement.registrationChildManagement.RegistrationChild;
 import java.util.List;
 
-public interface IAccessFacade 
-{
-	public int getParentId(int childId);
-	public int getNumberOfChildren(int parentId) throws SQLException;
-	public boolean isSick(RegistrationChild c);
-        public Account insert(Account pAccount);
-        public Account update(Account pAccount);
-        public List<Account> search(Account pAccount);
-        public Account delete(Account pAccount);
-        
-        public Account getParentById(int pParentId) throws SQLException;
+public interface IAccessFacade {
 
+    public int getParentId(int childId);
+
+    public int getNumberOfChildren(int parentId) throws SQLException;
+
+    public boolean isSick(RegistrationChild c);
+
+    public Account insert(Account pAccount);
+
+    public Account update(Account pAccount);
+
+    public List<Account> search(Account pAccount);
+
+    public Account delete(Account pAccount);
+
+    public Account getParentById(int pParentId) throws SQLException;
+    
+    public List<ClassBean> getClasses() throws SQLException;
+    
+    public List<RegistrationChild> search(RegistrationChild pRegistrationChild) throws SQLException;
 }
