015bc49ef20220838c440a8cc9e46081060932d7
navipar@gmail.com <navipar@gmail.com@a86227fa-c5a3-d7f0-fd0c-0a7fbeb29eb0>
2012-12-12 19:29:16 +0000

    Piccolo fix sulla visualizzazione degli ultimi menù della mensa

--- kids/src/it/unisa/kids/serviceManagement/canteenManagement/servlet/GetDailyMenuTableServlet.java
+++ kids/src/it/unisa/kids/serviceManagement/canteenManagement/servlet/GetDailyMenuTableServlet.java
@@ -88,7 +88,7 @@ public class GetDailyMenuTableServlet extends HttpServlet {
                     return;
                 }
             } else if (request.getParameter("onlyLastDailyMenu") != null) {
-                menuList = canteenManager.getLastMenu(10, MenuBean.DAILY_MENU);
+                menuList = canteenManager.getLastMenu(10, MenuBean.DAILY_MENU, true);
             } else {
                 sendMessageRedirect(request, response, "Errore: impossibile recuperare la lista dei men&ugrave;");
                 return;
